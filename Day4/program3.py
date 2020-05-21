05e45ac862e2ca97ddec590c70fdc67dee376d82
Shaksham Agarwal <57569810+sam31072000@users.noreply.github.com>
2020-05-21 22:23:21 +0530

    Update program3.py

--- Day4/program3.py
+++ Day4/program3.py
@@ -1,3 +1,2 @@
-dic = {1:2,2:4,3:9,24:676,4:16,5:25,6:36}
-sorted(dic.values())
-print(list(dic.values())[-2])
+dic = {1:2,2:4,3:9,24:676,4:16,5:25,6:36,7:49,25:625}
+print(sorted(dic.values())[-2])
